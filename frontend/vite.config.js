396c08a00af0173d47c975cf931d74786ac3b69d
Kiran Kumar Sharma <kirankumarsharma@Kirans-MacBook-Air.local>
2025-04-03 12:24:42 +0530

    project completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // Adjust size (default is 500 KB)
+  },
 })
+
+
